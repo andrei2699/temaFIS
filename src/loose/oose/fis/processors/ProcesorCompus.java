383bfe4ff4798ce8ac40bf0688b69117355840c4
Andrei Timar <atimar999@gmail.com>
2020-03-19 18:44:16 +0200

    added text

--- src/loose/oose/fis/processors/ProcesorCompus.java
+++ src/loose/oose/fis/processors/ProcesorCompus.java
@@ -13,12 +13,12 @@ public class ProcesorCompus implements Procesor {
 
     @Override
     public int proceseaza(ArrayList<Document> documente) {
-        int res = 0;
+        int asdf = 0;
 
         for (Procesor procesor : procesoare) {
-            res += procesor.proceseaza(documente);
+            asdf += procesor.proceseaza(documente);
         }
 
-        return res;
+        return asdf;
     }
 }
